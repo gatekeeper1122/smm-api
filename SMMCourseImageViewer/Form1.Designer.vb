b206a20fb85b6553ec30a478b3b1069a257910dd
acnleditor2 <valentinvanelslandeacnl@gmail.com>
2017-09-19 20:49:29 -0500

    Add course name viewer!!!

--- SMMCourseImageViewer/Form1.Designer.vb
+++ SMMCourseImageViewer/Form1.Designer.vb
@@ -26,13 +26,15 @@ Partial Class Form1
         Me.Button1 = New System.Windows.Forms.Button()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.Label1 = New System.Windows.Forms.Label()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PictureBox1
         '
-        Me.PictureBox1.Location = New System.Drawing.Point(212, 3)
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(320, 240)
         Me.PictureBox1.TabIndex = 0
@@ -40,33 +42,59 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(538, 215)
+        Me.Button1.Location = New System.Drawing.Point(-1, 350)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(182, 23)
+        Me.Button1.Size = New System.Drawing.Size(183, 22)
         Me.Button1.TabIndex = 1
         Me.Button1.Text = "View"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(0, 218)
+        Me.TextBox1.Location = New System.Drawing.Point(0, 329)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(206, 20)
+        Me.TextBox1.Size = New System.Drawing.Size(181, 20)
         Me.TextBox1.TabIndex = 2
         '
         'PictureBox2
         '
-        Me.PictureBox2.Location = New System.Drawing.Point(0, 242)
+        Me.PictureBox2.Location = New System.Drawing.Point(0, 246)
         Me.PictureBox2.Name = "PictureBox2"
         Me.PictureBox2.Size = New System.Drawing.Size(720, 81)
         Me.PictureBox2.TabIndex = 3
         Me.PictureBox2.TabStop = False
         '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.AllowWebBrowserDrop = False
+        Me.WebBrowser1.IsWebBrowserContextMenuEnabled = False
+        Me.WebBrowser1.Location = New System.Drawing.Point(700, 0)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.ScriptErrorsSuppressed = True
+        Me.WebBrowser1.ScrollBarsEnabled = False
+        Me.WebBrowser1.Size = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.TabIndex = 4
+        Me.WebBrowser1.Visible = False
+        Me.WebBrowser1.WebBrowserShortcutsEnabled = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(181, 330)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(516, 39)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Put a Course ID and press View!"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(720, 323)
+        Me.ClientSize = New System.Drawing.Size(720, 371)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.WebBrowser1)
         Me.Controls.Add(Me.PictureBox2)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button1)
@@ -87,4 +115,6 @@ Partial Class Form1
     Friend WithEvents Button1 As Button
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents WebBrowser1 As WebBrowser
+    Friend WithEvents Label1 As Label
 End Class
--- SMMCourseImageViewer/Form1.vb
+++ SMMCourseImageViewer/Form1.vb
@@ -5,8 +5,14 @@ Public Class Form1
         Try
             PictureBox1.Image = New Bitmap(New IO.MemoryStream(New WebClient().DownloadData("https://dypqnhofrd2x2.cloudfront.net/" & TextBox1.Text & ".jpg")))
             PictureBox2.Image = New Bitmap(New IO.MemoryStream(New WebClient().DownloadData("https://dypqnhofrd2x2.cloudfront.net/" & TextBox1.Text & "_full.jpg")))
+            Label1.Text = "Getting name..."
+            WebBrowser1.Url = New Uri("https://supermariomakerbookmark.nintendo.net/courses/" & TextBox1.Text)
         Catch ex As Exception
             MsgBox("Invalid Course ID!")
         End Try
+    End Sub
+
+    Private Sub CourseData(sender As Object, e As WebBrowserNavigatedEventArgs) Handles WebBrowser1.Navigated
+        Label1.Text = WebBrowser1.DocumentTitle.Replace("SUPER MARIO MAKER BOOKMARK | ", "").Replace(TextBox1.Text, "").Replace(" - ", "")
     End Sub
 End Class
